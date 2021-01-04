b979389ceb0e61dd7ab419609eda6d1735273f21
RecordBreaker12 <50182908+RecordBreaker12@users.noreply.github.com>
2021-01-04 08:40:22 -0300

    Add files via upload

---
+++ Leitor.java
@@ -0,0 +1,68 @@
+package maquina.de.turing;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Leitor {
+    
+    String funcao, entrada, saida;
+    static BufferedReader func;
+    static BufferedReader ent;
+    static BufferedWriter ext;
+    static String linha[];
+    static String posicaoAtual = "q0";
+    public Leitor(String funcao, String entrada, String saida) throws FileNotFoundException, IOException{
+        this.funcao = funcao;
+        this.entrada = entrada;
+        this.saida = saida;
+        Leitor.func = new BufferedReader(new FileReader(funcao));
+        Leitor.ent = new BufferedReader(new FileReader(entrada));
+        Leitor.ext = new BufferedWriter(new FileWriter(saida));
+    }
+    
+    public static String lerEntrada() throws IOException{
+        String leitor;
+        leitor = ent.readLine();
+        return leitor;
+    }
+ 
+    public int verificar() throws IOException{
+        String input[];
+        int i = 0;
+        input = lerEntrada().split("");
+        String leitor = "";
+        while (leitor != null) {
+            func.mark(0);
+            if(i>=input.length&&posicaoAtual.contains("qf")){
+                for (String input1 : input) {
+                    ext.append(input1);
+                }
+                func.close();
+                ent.close();
+                ext.close();
+                return 1;
+            }
+            linha = leitor.split(" ");
+            if(linha[0].equals(posicaoAtual) && linha[1].equals(input[i])){
+                posicaoAtual=linha[4];
+                input[i]=linha[2];
+                if(linha[3].equals("R")){
+                    i++;
+                }
+                else{
+                    i--;
+                }
+                func.reset();
+            }
+            else{
+                leitor = func.readLine();
+            }
+        }
+        return 0;
+    }
+    
+}
---
+++ MaquinaDeTuring.java
@@ -0,0 +1,16 @@
+package maquina.de.turing;
+
+import java.io.IOException;
+
+public class MaquinaDeTuring {
+
+    public static void main(String[] args) throws IOException {
+        String funcao = "C:\\Users\\Usuario\\Documents\\NetBeansProjects\\Maquina de Turing\\src\\maquina\\de\\turing\\funcao.txt";
+        String entrada = "C:\\Users\\Usuario\\Documents\\NetBeansProjects\\Maquina de Turing\\src\\maquina\\de\\turing\\entrada.txt";
+        String saida = "C:\\Users\\Usuario\\Documents\\NetBeansProjects\\Maquina de Turing\\src\\maquina\\de\\turing\\saída.txt";
+        Leitor l = new Leitor(funcao, entrada, saida);
+        String read = "a", direction = "R";
+        System.out.println(l.verificar());
+    }
+    
+}
